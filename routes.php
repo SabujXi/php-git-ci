e3efef6b1c2433bd10d6a882c7d205a059ffd5d0
sabuj <md.sabuj.sarker@gmail.com>
2019-06-15 18:49:12 +0600

    Navbar auth, auth guard for routes

--- routes.php
+++ routes.php
@@ -8,11 +8,11 @@ $app->route('/login', 'Login', ['name' => 'login']);
 $app->route('/logout', 'Login@logout', ['name' => 'logout']);
 $app->route('/auth_test', 'Login@auth_test', ['name' => 'auth_test']);
 $app->route('/auth_setup', 'AuthSetup', ['name' => 'auth_setup']);
-$app->route('/deploy_settings', 'DeploySettings', ['name' => 'deploy_settings']);
-$app->route('/deploy', 'Deploy@deploy', ['name' => 'deploy']);
-$app->route('/reset_n_deploy', 'Deploy@reset_n_deploy', ['name' => 'reset_n_deploy']);
-$app->route('/upload_data', 'DataManager@upload', ['name' => 'upload_data']);
-$app->route('/execute_command', 'ExecuteCommand@execute', ['name' => 'execute_command']);
+$app->route('/deploy_settings', 'DeploySettings', ['name' => 'deploy_settings', 'auth_needed' => true]);
+$app->route('/deploy', 'Deploy@deploy', ['name' => 'deploy', 'auth_needed' => true]);
+$app->route('/reset_n_deploy', 'Deploy@reset_n_deploy', ['name' => 'reset_n_deploy', 'auth_needed' => true]);
+$app->route('/upload_data', 'DataManager@upload', ['name' => 'upload_data', 'auth_needed' => true]);
+$app->route('/execute_command', 'ExecuteCommand@execute', ['name' => 'execute_command', 'auth_needed' => true]);
 
 
 /* closure example
