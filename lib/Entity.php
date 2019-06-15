bc68862d7ee10556ff728bf3e9923ee3f5595721
sabuj <md.sabuj.sarker@gmail.com>
2019-06-15 15:49:58 +0600

    Entity append, clear and key splitting improvement, DRY

--- lib/Entity.php
+++ lib/Entity.php
@@ -20,30 +20,50 @@ class Entity
         $this->entity = $json_array;
     }
 
-    public function get($key, $default=null){
+    private function split_key($key){
         $_keys = explode('.', $key);
-        $key = array_pop($_keys);
-        $arr = $this->entity;
-        foreach ($_keys as $_key){
-            $arr = $arr[$_key];
-        }
+        $final_key = array_pop($_keys);
+        $keys = new \stdClass();
+        $keys->final_key = $final_key;
+        $keys->keys = $_keys;
+        return $keys;
+    }
 
-        if(isset($arr[$key])){
-            return $arr[$key];
+    private function &get_array_by_key($keys){
+        $arr = &$this->entity;
+        foreach ($keys as $key){
+            $arr = &$arr[$key];
+        }
+        assert(is_array($arr));
+        return $arr;
+    }
+
+    public function get($key, $default=null){
+        $keys = $this->split_key($key);
+        $arr = &$this->get_array_by_key($keys->keys);
+        if(isset($arr[$keys->final_key])){
+            return $arr[$keys->final_key];
         }else{
             return $default;
         }
     }
 
     public function set($key, $value){
-        $_keys = explode('.', $key);
-        $key = array_pop($_keys);
-        $arr = &$this->entity;
-        foreach ($_keys as $_key){
-            $arr = &$arr[$_key];
-        }
+        $keys = $this->split_key($key);
+        $arr = &$this->get_array_by_key($keys->keys);
+        $arr[$keys->final_key] = $value;
+    }
 
-        $arr[$key] = $value;
+    public function append($key, $value){
+        $keys = $this->split_key($key);
+        $arr = &$this->get_array_by_key($keys->keys);
+        $arr[] = $value;
+    }
+
+    public function clear($key){
+        $keys = $this->split_key($key);
+        $arr = &$this->get_array_by_key($keys->keys);
+        $arr = [];
     }
 
     public function __get($name) {
